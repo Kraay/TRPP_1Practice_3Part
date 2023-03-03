42229638a60d5cb374acb4607d2fe246ab0e3bea
Dominik <dominik-yadikaryan@mail.ru>
2023-03-03 17:12:34 +0300

    Second commit for Second_Branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TRPP_1Practice_3Part-------First_Branch");
         System.out.println("Some code for First commit");
+        System.out.println("Some code to Second commit");
     }
 }
